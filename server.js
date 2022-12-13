25209e75b378e917c5b9dc61de59cb2e6fec75ea
hamitsehjal <hamitsehjal26@gmail.com>
2022-12-12 20:59:40 -0500

    JWT-Tutorial - Node.js

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const jwt = require("jsonwebtoken")
+const app = express()
+
+require("dotenv").config();
+
+
+const posts = [
+    {
+        username: "Kyle",
+        title: "Post 1"
+    },
+    {
+        username: "Jim",
+        title: "Post 2"
+    }
+]
+
+app.use(express.json())
+// Designing the middleware
+const authenticiateToken=(req,res,next)=>{
+    const authHeader=req.headers['authorization']
+    const token=authHeader&&authHeader.split(" ")[1]
+
+    if(token==null)
+    {
+        return res.sendStatus(401)
+
+    }
+
+    jwt.verify(token,process.env.ACCESS_SECRET,(err,user)=>{
+        if(err)
+        {
+            return res.sendStatus(403)
+        }
+
+        req.user=user
+        next()
+    })
+
+}
+app.get("/posts",authenticiateToken, (req, res) => {
+    res.json(posts.filter(post=>post.username==req.user.name))
+})
+
+app.post("/login",(req,res)=>{
+
+    // Authenticiate the User
+
+    const username=req.body.username
+    const user={name:username}
+
+    const accessToken=jwt.sign(user,process.env.ACCESS_SECRET)
+    res.json({accessToken:accessToken})
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Express Server listening on Port: 3000")
+})
